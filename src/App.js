3b95de364ead2737d72de1e7c2f585427f03d453
Haneef0123 <bashahaneef103@gmail.com>
2021-07-12 13:56:26 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import AppHeader from './components/common/header';
+import AppHome from './views/home';
+import { Layout} from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import AppFooter from './components/common/footer';
+const { Header,Content,Footer} = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout className="mainLayout" >
+        <Header>
+          <AppHeader/>    
+        </Header>
+        <Content>
+          <AppHome/>
+        </Content> 
+        <Footer>
+          <AppFooter/>
+        </Footer>
+      </Layout>   
   );
 }
 
---
+++ src/components/home/contact.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+const { TextArea } = Input;
+
+
+function AppContact() {
+    return (
+        <div id="contact" className="block contactBlock bgGray">
+          <div className="container-fluid">
+            <div className="titleHolder">
+              <h2>Get in Touch</h2>
+              <p>Debitis itaque minima dolorum fuga aperiam inventore, quidem enim</p>
+            </div>
+            <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+    //   onFinish={onFinish}
+    >
+      <Form.Item
+        name="fullname"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Fullname!',
+          },
+        ]}
+      >
+        <Input 
+        // prefix={<UserOutlined className="site-form-item-icon" />} 
+        placeholder="Fullname" />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Email Address!',
+          },
+        ]}
+      >
+        <Input
+        //   prefix={<LockOutlined className="site-form-item-icon" />}
+          type="email"
+          placeholder="Email Address"
+        />
+      </Form.Item>
+      <Form.Item
+        name="telephone"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Telephone Number!',
+          },
+        ]}
+      >
+        <Input 
+        // prefix={<UserOutlined className="site-form-item-icon" />} 
+        placeholder="Telephone" />
+      </Form.Item>
+      <Form.Item
+        name="subject"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Subject!',
+          },
+        ]}
+      >
+        <Input 
+        // prefix={<UserOutlined className="site-form-item-icon" />} 
+        placeholder="Subject" />
+      </Form.Item>
+      <Form.Item
+        name="message"
+      >
+        <TextArea 
+        // prefix={<UserOutlined className="site-form-item-icon" />} 
+        placeholder="Message" />
+      </Form.Item>
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+          },
+        ]}
+        
+      >
+        <Checkbox>
+        I agree with terms and conditions.
+        </Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Submit
+        </Button>
+        
+      </Form.Item>
+    </Form>
+          </div>
+        </div> 
+    )
+}
+
+export default AppContact;
